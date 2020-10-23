dea201cfd835f5b40a4b28055a3ef10affd31532
Joshua77@gmail.com <joshdavis>
2020-10-23 10:32:43 -0400

    Initial commit
    Create shareable orghierarchy table for PSM

--- R/psm_sch/scm_mer_sitelevel.R
+++ R/psm_sch/scm_mer_sitelevel.R
@@ -2,6 +2,7 @@
 ##  Created: 2.7.19 - j davis
 ##  Updated: 2.24.19
 ##        Updated: 7.2.2020 by MH- taking over this task
+##        Updated: 10.21.20 to take advantage of genie upgrade
 ##  Create site-level datasets for SCM division
 ##  At the request of Julia Bem and Meaghan Douglass, and approved by r lucas on 2/6/2019,
 ##  create-site level datasets for SCM division to pass to contractor for supply chain,
@@ -29,10 +30,10 @@ library(glamr)
 indc <- c("HTS_TST", "HTS_TST_POS", "HTS_TST_NEG", "TX_CURR", "TX_NEW", "TX_PVLS")
 
 ##  site level data goes here
-input <- "msd_here"
+input <- "C:/Users/Josh/Documents/data/fy20_q3_v1/psm/mer_psm"
 
 ## where you want the output
-output <- "where you want files to go"
+output <- "C:/Users/Josh/Documents/data/fy20_q3_v1/psm/mer_psm/out"
 
 ## list of OUs per Nagesh on 3/25 and approved by SCM
 OUs <- c("Angola",
@@ -58,7 +59,7 @@ OUs <- c("Angola",
 ##  create rds files from site level
 
 ## create obj that is list of files
-makey_rds <- dir(input, pattern = "*.zip", full.names = TRUE)
+makey_rds <- dir(input, pattern = "*.txt", full.names = TRUE)
 
 ##  create function
 site.msd <- function(file) {
@@ -70,14 +71,14 @@ purrr::map( .x = makey_rds, .f = ~site.msd(.x))
 #-----------------------------------------------------------------------------------------
 ##  create master scm dataset
 
-rdss <- dir(input, pattern = "*rds", full.names = TRUE)
+files <- dir(input, pattern = "*txt", full.names = TRUE)
 
 ##  create function to subset
 
 ##
 get_scm <- function(input) {
   
-  df_mer <- readr::read_rds(input) %>% 
+  df_mer <- ICPIutilities::(input) %>% 
     dplyr::filter(fiscal_year == 2020,
                   countryname %in% c(OUs),
                   (indicator %in% indc &
@@ -102,6 +103,42 @@ readr::write_csv(big_df, file.path(output, "mer_fy20_q1_q2_site_scm.csv"))
 big_df %>% dplyr::distinct(standardizeddisaggregate) %>% dplyr::arrange(standardizeddisaggregate) %>% print(n=Inf)
 big_df %>% dplyr::distinct(operatingunit) %>% dplyr::arrange(operatingunit) %>% print(n=Inf)
 big_df %>% dplyr::distinct(indicator) %>% dplyr::arrange(indicator) %>% print(n=Inf)
+
+
+#test-----------------------------------------------------
+input <- "C:/Users/Josh/Documents/data/fy20_q3_v1/psm/mer_psm/Genie_SITE_IM_Angola_Frozen_97fcc5e8-68a0-4d41-9d3a-f6a4c022b5c4.txt"
+
+
+df_mer <- ICPIutilities::read_msd(input) %>%
+  ICPIutilities::reshape_msd("long") %>% 
+  dplyr::filter(period == 'fy2020q3',
+                countryname %in% c(OUs),
+                (indicator %in% indc &
+                   numeratordenom == "N" & trendscoarse %in% c("<15", "15+")) |
+                  (indicator %in% indc & 
+                     standardizeddisaggregate == "Total Numerator")) %>% 
+  dplyr::group_by_at(vars(-primepartner, -fundingagency, -mech_code, -mech_name,
+                          -pre_rgnlztn_hq_mech_code, -prime_partner_duns, -award_number,
+                          -ageasentered, -trendsfine, -trendssemifine, -sex, -categoryoptioncomboname)) %>%
+  dplyr::summarise(across(where(is.numeric), sum, na.rm = TRUE)) %>% 
+  dplyr::ungroup() %>% 
+  spread(period, val)
+
+
+df_mer <- ICPIutilities::read_msd(input) %>%
+  ICPIutilities::reshape_msd("long") %>% 
+  dplyr::filter(period == 'fy2020q3',
+                countryname %in% c(OUs),
+                (indicator %in% indc &
+                   numeratordenom == "N" & trendscoarse %in% c("<15", "15+")) |
+                  (indicator %in% indc & 
+                     standardizeddisaggregate == "Total Numerator")) %>% 
+  dplyr::group_by_at(vars(orgunituid, sitename, operatingunit, countryname, snu1, psnu, facility, sitetype,
+                          indicator, standardizeddisaggregate, trendscoarse, period)) %>%
+  dplyr::summarise(across(where(is.numeric), sum, na.rm = TRUE)) %>% 
+  dplyr::ungroup() %>% 
+  spread(period, val)
+
 
   
 
